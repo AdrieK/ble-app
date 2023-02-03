8f96c883938148e86103c8457ed95e45f841dfdf
Adrie <a.kooijman@gmail.com>
2023-02-03 17:09:16 +0100

    Added cookie-functions

--- docs/js/buttons.js
+++ docs/js/buttons.js
@@ -1,6 +1,5 @@
 // Button definitions and button (gui) functions
-
-console.log("buttons.js script");
+// console.log("buttons.js script");
 
 var connectButton = document.getElementById("connect_button");
 var listButton = document.getElementById("list_button");
@@ -64,13 +63,40 @@ function testPressed() {
     const timestamp = new Date().getTime();
     saveButton.disabled = false;
 	listButton.disabled = false;
-	noLoop();
+	noLoop(); // p5.js
 }
 
 function debugPressed() {
     console.log('debug pressed');
+	ck = document.cookie;
+	console.log('ck',ck);
 	debug = !debug;
     const timestamp = new Date().getTime();
     saveButton.disabled = false;
-    listButton.disabled = false;
+	listButton.disabled = false;
+	//noLoop(); p5.js
+}
+
+// https://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
